d9d1684937b01cb00c9cb99af4f399659ce94c67
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-04-21 15:24:58 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const assert = require('assert');
-eval(fs.readFileSync('code.js')+'');
-
-assert(factorial(10) === 3628800);
-assert(factorial(1) === 1); 
-assert(factorial(5) === 120);  
+eval(fs.readFileSync('code.js')+''); 
 
 assert(e(10) === 2.7182818011463845);
 assert(e(1) === 2); 
